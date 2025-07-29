02bcec738281d3fe10d27d835fe204a552c17657
jameelf22labs <jameel.m@f22labs.com>
2025-07-29 17:40:00 +0530

    remove props

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -13,30 +13,13 @@ import {
   Team,
 } from "./components";
 
-import amazon from "../,,/../../assets/images/amazon.svg";
-import dribble from "../,,/../../assets/images/dribble.svg";
-import hubspot from "../,,/../../assets/images/hubspot.svg";
-import notion from "../,,/../../assets/images/notion.svg";
-import netflix from "../,,/../../assets/images/netflix.svg";
-import zoom from "../,,/../../assets/images/zoom.svg";
-
 const Home = (): JSX.Element => {
-  const customers = React.useMemo(() => {
-    return [
-      { imageUrl: amazon },
-      { imageUrl: dribble },
-      { imageUrl: hubspot },
-      { imageUrl: notion },
-      { imageUrl: netflix },
-      { imageUrl: zoom },
-    ];
-  }, []);
   return (
     <React.Fragment>
       <div className={Style.homeContainer}>
         <Navbar />
         <Hero />
-        <Customer customers={customers} />
+        <Customer />
         <Service />
         <CaseStudies />
         <OurWorkingProcess />
--- src/features/home/components/customer/Customer.tsx
+++ src/features/home/components/customer/Customer.tsx
@@ -1,19 +1,35 @@
 import type { JSX } from "react";
 import React from "react";
 
-export interface CustomerProps {
-  customers: Array<{ imageUrl: string; name?: string }>;
-}
+import amazon from "../,,/../../assets/images/amazon.svg";
+import dribble from "../,,/../../assets/images/dribble.svg";
+import hubspot from "../,,/../../assets/images/hubspot.svg";
+import notion from "../,,/../../assets/images/notion.svg";
+import netflix from "../,,/../../assets/images/netflix.svg";
+import zoom from "../,,/../../assets/images/zoom.svg";
 
-const Customer = ({ customers }: CustomerProps): JSX.Element => {
+const Customer = (): JSX.Element => {
+  const customers = React.useMemo(() => {
+    return [
+      { imageUrl: amazon, name: "amazon" },
+      { imageUrl: dribble, name: "dribble" },
+      { imageUrl: hubspot, name: "hubspot" },
+      { imageUrl: notion, name: "notion" },
+      { imageUrl: netflix, name: "netflix" },
+      { imageUrl: zoom, name: "zoom" },
+    ];
+  }, []);
   return (
-    <React.Fragment>
-      <div className="flex justify-center gap-20">
-        {customers.map((customer) => {
-          return <img src={customer.imageUrl} alt={customer.name} width="150px" height="150px" />;
-        })}
-      </div>
-    </React.Fragment>
+    <div className="flex flex-wrap justify-center gap-8 sm:gap-12 md:gap-16 lg:gap-20 px-4 py-6">
+      {customers.map((customer, index) => (
+        <img
+          key={index}
+          src={customer.imageUrl}
+          alt={customer?.name ?? `customer-${index}`}
+          className="w-24 sm:w-28 md:w-32 lg:w-36 h-auto object-contain"
+        />
+      ))}
+    </div>
   );
 };
 
